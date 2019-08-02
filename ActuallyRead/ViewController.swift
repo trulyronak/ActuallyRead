e15018c7c55de1bebfb97b6532a3b4cbe3e21671
Ronak Shah <ronakshahultimate@gmail.com>
2019-08-02 08:41:49 -0400

    Initial Commit

---
+++ ActuallyRead/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActuallyRead
+//
+//  Created by Ronak Shah on 8/2/19.
+//  Copyright © 2019 Ronak Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
